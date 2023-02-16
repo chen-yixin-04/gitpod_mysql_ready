492600c5c829939a4cfe07cd494b1fcd84b3de10
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2023-02-16 11:49:46 +0000

    3

--- main_2.py
+++ main_2.py
@@ -9,4 +9,23 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE customers3 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+sql = "INSERT INTO customers3 (name, address) VALUES (%s, %s)"
+val = [("John", "Highway 21"),
+       ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+                            ]
+mycursor.executemany(sql, val)
+
+mydb.commit()
+print("1 record inserted, ID:", mycursor.lastrowid)
