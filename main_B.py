ea5ec132fae87e385b66ceffa9d0301e07dddd02
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2023-03-08 12:00:48 +0000

    B

--- main_B.py
+++ main_B.py
@@ -7,10 +7,15 @@ mydb = mysql.connector.connect(
   database="Animali"
 )
 
+mycursor = mydb.cursor()
+
 sql = "INSERT INTO mammiferi (Id, Nome_Proprio, Razza, Peso, Eta) VALUES (%s,%s,%s,%s,%s)"
-val = [("1", "gatto","ragdoll","5 kg","10")
-       ("2", "cane","barboncino","3 kg","2")
-       ("3", "cavia","peruviana","1 kg","3")
-       ("4", "criceto","siriano","0.5 kg","1")
-       ("5", "elefante","africano","3000 kg","20")
+val = [("1", "gatto","ragdoll",5,10),
+       ("2", "cane","barboncino",3,2),
+       ("3", "cavia","peruviana",1,3),
+       ("4", "criceto","siriano",1,1),
+       ("5", "elefante","africano",3,20)
 ]
+mycursor.executemany(sql, val)
+
+mydb.commit()
